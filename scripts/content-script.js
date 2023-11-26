15bad670ca21dac66cc079f003cbd717ca4518e5
MadLadMadara <s.aj.mcruvie@gmail.com>
2023-11-26 14:51:31 +0000

    TTS reading with highlighting in element click

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,15 +1,29 @@
-let port = chrome.runtime.connect({name: "tts"});
-
 let currentElement = null;
-
-// TTS message receiver port
-port.onMessage.addListener(function(msg) {
-
-});
 
 // Events on the page
 $(document).on('click', function(e) {
-    currentElement = e.target;
-    port.postMessage({action: "read", text: currentElement.textContent});
-
+    currentElement = e.target; 
+    console.log("Clicked: " + $(currentElement).text());
+    chrome.runtime.sendMessage(
+        {
+            action: "read",
+            text: currentElement.textContent
+        }
+    );
 });
+
+// TTS message receiver from service-worker.js
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        // highlight-word word in current element
+        if (request.action === "highlight-word") {
+            // Get the text content of the element
+            $(document).unmark();
+            var startIndex = request.charIndex;
+            var wordLength = request.length;
+            console.log("Request" + request.action + " " + startIndex + " " + wordLength);
+            console.log("Highlighting: ");
+            $(currentElement).markRanges([{ start: startIndex, length: wordLength }]);
+        }
+    }
+);
--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,42 @@
-// TTS message receiver port
-chrome.runtime.onConnect.addListener(function(port) {
-    if (port.name !== "tts") {
-        return;
-    }
-    port.onMessage.addListener(function(msg) {
-        if (msg.action === "read") {
-            chrome.tts.speak(msg.text, 
-                    { rate: 0.8, onEvent: function(event) {}}, function() {});
+// TTS message receiver from content-script.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    chrome.tabs.query({active : true, lastFocusedWindow : true}, function (tabs) {
+        var CurrTab = tabs[0];
+
+        if (request.action === "read") {  
+            chrome.tts.speak(
+                request.text, 
+                {
+                    voiceName: "Microsoft George - English (United Kingdom)",
+                    rate: 0.8, 
+                    lang: 'en-GB',
+                    onEvent: function(event) 
+                    {
+                        console.log("TTS event: " + event.type);
+                        switch(event.type) {
+                            case 'start':
+                                break;
+                            case 'word':
+                                console.log("TTS read event data: " + event.charIndex + " " + event.length);
+                                chrome.tabs.sendMessage(CurrTab.id, 
+                                    {
+                                        action: "highlight-word",
+                                        charIndex: event.charIndex, 
+                                        length: event.length
+                                    });
+                                break;
+                            case 'interrupted':
+                                break;
+                            case 'cancelled':
+                                break;
+                            case 'error':
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }, 
+                function() {});
         }
-    });
+    })
 });
