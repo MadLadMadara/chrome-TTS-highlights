3eea282801baae24d78c0f59360b845349d09c2d
MadLadMadara <s.aj.mcruvie@gmail.com>
2023-11-28 17:30:55 +0000

    dynamically load content script on update/install

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,5 +1,5 @@
+// element to be read aloud by TTS
 let currentElement = null;
-
 // Events on the page
 $(document).on('click', function(e) {
     currentElement = e.target; 
@@ -12,18 +12,25 @@ $(document).on('click', function(e) {
     );
 });
 
-// TTS message receiver from service-worker.js
+// Chrome message receiver
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        // highlight-word word in current element
-        if (request.action === "highlight-word") {
-            // Get the text content of the element
-            $(document).unmark();
-            var startIndex = request.charIndex;
-            var wordLength = request.length;
-            console.log("Request" + request.action + " " + startIndex + " " + wordLength);
-            console.log("Highlighting: ");
-            $(currentElement).markRanges([{ start: startIndex, length: wordLength }]);
+    (request) => {
+        switch(request.action) {
+            case "mark-word":
+                $(document).unmark();
+                var startIndex = request.charIndex;
+                var wordLength = request.length;
+                console.log("Request" + request.action + " " + startIndex + " " + wordLength);
+                console.log("Highlighting: ");
+                $(currentElement).markRanges([{ start: startIndex, length: wordLength }]);
+                break;
+            case "unmark-all":
+                $(document).unmark();
+                break;
+            default:
+                $(document).unmark();
+                console.error("Unknown action: " + request.action);
+                break;
         }
     }
 );
--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,92 @@
-// TTS message receiver from content-script.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    chrome.tabs.query({active : true, lastFocusedWindow : true}, function (tabs) {
-        var CurrTab = tabs[0];
+// message all tabs
+const messageAllTabs = (message) => {
+    chrome.tabs.query({}, (tabs) => tabs.forEach( tab => {
+        
+    }));
+}
+// on install and update listener
+chrome.runtime.onInstalled.addListener((details) => {
+    // one install or update
+    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL || 
+        details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+            console.log("Installing or updating...");
+        // inject scripts to all tabs
+        chrome.tabs.query({url:"*://*/*"}, (tabs) => tabs.forEach( tab => {
+            // ignore chrome:// pages
+            console.log("Injecting scripts to tab: " + tab.url);
+            if (tab.url?.startsWith("chrome://")) return;
+            chrome.scripting.executeScript({
+                target: {
+                    tabId: tab.id
+                },files: [
+                    "thirdPartyScripts/jquery-3.7.1.min.js",
+                    "thirdPartyScripts/jquery.mark.min.js",
+                    "scripts/content-script.js"
+                ]
+            });
+        }, (error) => {
+            console.error("Error injecting scripts on install/update: " + error);
+        }));
+    }
+    // one install or update
+    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+      
 
-        if (request.action === "read") {  
+    }else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+
+    }
+});
+
+/// Chrome message receiver
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    var CurrTab = sender.tab;
+    switch(request.action) {  
+        case "read":
             chrome.tts.speak(
                 request.text, 
                 {
                     voiceName: "Microsoft George - English (United Kingdom)",
                     rate: 0.8, 
                     lang: 'en-GB',
-                    onEvent: function(event) 
-                    {
-                        console.log("TTS event: " + event.type);
+                    onEvent: (event) => {
                         switch(event.type) {
                             case 'start':
                                 break;
                             case 'word':
-                                console.log("TTS read event data: " + event.charIndex + " " + event.length);
+                                console.log("TTS word: " + event.charIndex + " " + event.length);
                                 chrome.tabs.sendMessage(CurrTab.id, 
                                     {
-                                        action: "highlight-word",
+                                        action: "mark-word",
                                         charIndex: event.charIndex, 
                                         length: event.length
                                     });
                                 break;
+                            case 'end':
+                                console.log("TTS end")
+                                messageAllTabs({action: "unmark-all"});
+                                break;
                             case 'interrupted':
+                                console.log("TTS interrupted");
+                                messageAllTabs({action: "unmark-all"});
                                 break;
                             case 'cancelled':
+                                console.log("TTS cancelled");
+                                messageAllTabs({action: "unmark-all"});
                                 break;
                             case 'error':
                                 break;
                             default:
+                                // as a fail safe, unmark all
+                                // messageAllTabs({action: "unmark-all"});
+                                console.error("Unknown TTS event: " + event.type);
                                 break;
                         }
                     }
                 }, 
                 function() {});
-        }
-    })
+                break;
+        default:
+            console.error("Unknown action in message: " + request.action);
+            break;
+    }
 });
