b7cec5ff009b56ab7e155862a7f38084547af92a
MadLadMadara <s.aj.mcruvie@gmail.com>
2024-02-06 15:45:01 +0000

    JSDOM & mozilla/readability lib working in service-worker

--- content-script.js
+++ content-script.js
@@ -1,11 +1,10 @@
 (async () => {
     // main body, required for top level await of imports
     console.log("content-script.js loaded");
-    await import(chrome.runtime.getURL("/node_modules/jquery/dist/jquery.js"));
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         switch (request.action) {
-            case "get_treeWalker_all":
-                sendResponse(getAllRelevantDomElements());
+            case "get_dom_text":
+                sendResponse(document.all[0].outerHTML);
                 break;
             default:
                 break;
@@ -13,44 +12,3 @@
     });
 })();
 
-function getAllRelevantDomElements(root = document) {
-    const treeWalker = document.createTreeWalker(
-        root,
-        NodeFilter.SHOW_ELEMENT,
-        {
-            acceptNode(node) {
-                console.log(node.role);
-                if (
-                    (node.tagName === "TITLE" ||
-                        node.tagName === "HEADER" ||
-                        node.tagName === "MAIN" ||
-                        node.tagName === "SECTION" ||
-                        node.tagName === "ARTICLE") &&
-                    node.textContent.trim().length > 0 &&
-                    node.hidden === false
-                ) {
-                    return NodeFilter.FILTER_ACCEPT;
-                }
-                return NodeFilter.FILTER_SKIP;
-            },
-        }
-    );
-
-    
-    let elements = [];
-    while (treeWalker.nextNode()) {
-        let children = [];
-        treeWalker.currentNode.childNodes.forEach((child) => {
-            children.push({
-                tagName: child.tagName,
-                textContent: child.textContent,
-            });
-        });
-
-        elements.push({
-            tagName: treeWalker.currentNode.tagName,
-            childNodes: children,
-        });
-    }
-    return elements;
-}
--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,20 @@
-// local variables
-
-
+// imports and local variables
 console.log("service-worker.js loaded");
-import { ttsConfig } from "./locales.js";
-import isGibberish from "/node_modules/gibberish-detector/index.js";
+import Readability from "/node_modules/@mozilla/readability/Readability.js";
+import isProbablyReaderable from "/node_modules/@mozilla/readability/Readability-readerable.js";
+import {JSDOM} from '/lib/jsdom.js';         
+
 // required for updating/installing content scripts
 const manifestData = chrome.runtime.getManifest();
+// var { Readability } = require('@mozilla/readability');
+// var { JSDOM } = require('jsdom');
+// var doc = new JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
+//   url: "https://www.example.com/the-page-i-got-the-source-from"
+// });
+// let reader = new Readability(doc.window.document);
+// let article = reader.parse();
+
+
 // on install
 chrome.runtime.onInstalled.addListener((details) => {
     // create context menu
@@ -22,19 +31,24 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             chrome.tabs.sendMessage(
                 tabs[0].id,
-                {action: "get_treeWalker_all" },
+                {action: "get_dom_text"},
                 (response) => {
-                    console.log(parseHTML(response));
+                    const document = new JSDOM(response, {url: tabs[0].url}).window.document;
+                    const reader = new Readability(document).parse();
+                    console.group("Readability");
+                    console.log(`Title: ${reader.title}`);
+                    console.log(`Byline: ${reader.byline}`);
+                    console.log(`Content: ${reader.content}`);
+                    console.log(`Text Content: ${reader.textContent}`);
+                    console.log(`Length: ${reader.length}`);
+                    console.log(`Excerpt: ${reader.excerpt}`);
+                    console.log(`Site Name: ${reader.siteName}`);
+                    console.log(`Direction: ${reader.dir}`);
+                    console.log(`Language: ${reader.lang}`);
+                    console.groupEnd();
                 }
             );
         });
     }
 });
-
-
-// TODO: add event listener for when active tab changes.
-
-const parseHTML = (treeWaler) => {
-    console.log(treeWaler);
-}
 
