5a522245591cbe7edc5d4773183c103520be25bd
MadLadMadara <s.aj.mcruvie@gmail.com>
2024-02-06 22:05:17 +0000

    Moved service-worker logic into side-panel script

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // imports and local variables
-console.log("service-worker.js loaded");
+
 import Readability from "/node_modules/@mozilla/readability/Readability.js";
 import isProbablyReaderable from "/node_modules/@mozilla/readability/Readability-readerable.js";
 import {JSDOM} from '/lib/jsdom.js';         
@@ -23,32 +23,51 @@ chrome.runtime.onInstalled.addListener((details) => {
         title: "Open TTS Highlighter Side Panel",
         contexts: ["all"],
     });
+    console.log("TTS Highlighter installed");
 });
 
 // on context menu click events
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     if (info.menuItemId === "openSidePanel") {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                {action: "get_dom_text"},
-                (response) => {
-                    const document = new JSDOM(response, {url: tabs[0].url}).window.document;
-                    const reader = new Readability(document).parse();
-                    console.group("Readability");
-                    console.log(`Title: ${reader.title}`);
-                    console.log(`Byline: ${reader.byline}`);
-                    console.log(`Content: ${reader.content}`);
-                    console.log(`Text Content: ${reader.textContent}`);
-                    console.log(`Length: ${reader.length}`);
-                    console.log(`Excerpt: ${reader.excerpt}`);
-                    console.log(`Site Name: ${reader.siteName}`);
-                    console.log(`Direction: ${reader.dir}`);
-                    console.log(`Language: ${reader.lang}`);
-                    console.groupEnd();
-                }
-            );
-        });
+        chrome.sidePanel.open({ windowId: tab.windowId });
     }
 });
+
+// chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+//     if (info.menuItemId === "openSidePanel") {
+//         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//             chrome.tabs.sendMessage(
+//                 tabs[0].id,
+//                 {action: "get_dom_text"},
+//                 (response) => {
+//                     const document = new JSDOM(response, {
+//                         url: tabs[0].url,
+//                         referrer: tabs[0].url,
+//                         contentType: "text/html",
+//                         includeNodeLocations: true,
+//                     }).window.document;
+//                     if (isProbablyReaderable(document)) {
+//                         console.log("This page is readerable");
+//                     } else {
+//                         console.log("This page is not readerable");
+//                     }
+//                     chrome.sidePanel.open({ windowId: tab.windowId });
+
+//                     const reader = new Readability(document).parse();
+//                     console.group("Readability");
+//                     console.log(`Title: ${reader.title}`);
+//                     console.log(`Byline: ${reader.byline}`);
+//                     console.log(`Content: ${reader.content}`);
+//                     console.log(`Text Content: ${reader.textContent}`);
+//                     console.log(`Length: ${reader.length}`);
+//                     console.log(`Excerpt: ${reader.excerpt}`);
+//                     console.log(`Site Name: ${reader.siteName}`);
+//                     console.log(`Direction: ${reader.dir}`);
+//                     console.log(`Language: ${reader.lang}`);
+//                     console.groupEnd();
+//                 }
+//             );
+//         });
+//     }
+// });
 
--- sidepanel/script.js
+++ sidepanel/script.js
@@ -1,1 +1,38 @@
+// imports
 import "/node_modules/jquery/dist/jquery.js";
+import Readability from "/node_modules/@mozilla/readability/Readability.js";
+import isProbablyReaderable from "/node_modules/@mozilla/readability/Readability-readerable.js";
+import { JSDOM } from "/lib/jsdom.js";
+// constants
+const manifestData = chrome.runtime.getManifest();
+// on panel open event
+(async () => {
+    // main body, required for top level await of imports
+    console.log("sidepanel/script.js loaded");
+    const tab = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.tabs.sendMessage(
+        tab[0].id,
+        { action: "get_dom_text" },
+        (response) => {
+            const document = new JSDOM(response, {
+                url: tab[0].url,
+                referrer: tab[0].url,
+                contentType: "text/html",
+                includeNodeLocations: true,
+            }).window.document;
+            if (isProbablyReaderable(document)) {
+                console.log("This page is readerable");
+            } else {
+                console.log("This page is not readerable");
+            }
+            chrome.sidePanel.open({ windowId: tab[0].windowId });
+            const reader = new Readability(document).parse();
+            console.group("Readability");
+            console.log(`Title: ${reader.title}`);
+            console.log(`Byline: ${reader.byline}`);
+            console.log(`Content: ${reader.content}`);
+            console.log(`Text Content: ${reader.textContent}`);
+            $("main").append(`${reader.content}`);
+        }
+    );
+})();
